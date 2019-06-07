442c3502a3c4271a4e61dac5b039973871cc5643
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-06 22:08:02 -0300

    first commit e criação da classe nó

---
+++ Labirinto/Labirinto/No.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labirinto
+{
+    public class No
+    {
+        public int g, h, f;
+        public No pai;
+        public Tuple<int, int> posicao;
+
+        public No(No pai=null, Tuple<int, int> posicao=null)
+        {
+            this.pai = pai;
+            this.posicao = posicao;
+
+            this.g = this.h = this.f = 0;
+        }
+
+        public bool equals(No outro)
+        {
+            return this.posicao == outro.posicao;
+        }
+    }
+}
